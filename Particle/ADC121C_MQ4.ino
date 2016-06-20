bcdb8d762ffe1ad140514976db742ecbf321ca15
Yadwinder Singh <ryker1990@gmail.com>
2016-06-20 16:10:21 +0530

    Update ADC121C_MQ4.ino

--- Particle/ADC121C_MQ4.ino
+++ Particle/ADC121C_MQ4.ino
@@ -11,7 +11,7 @@
 #define Addr 0x50
 
 int raw_adc = 0;
-float ppm = 0.0;
+double ppm = 0.0;
 void setup()
 {
   // Set variable
@@ -54,5 +54,5 @@ void loop()
 
   // Output data to dashboard
   Particle.publish("Methane concentration : ", String(ppm));
-  delay(500);
+  delay(1000);
 }
